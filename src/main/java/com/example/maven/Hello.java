d3f89c3d2108cac586fd9b8d8e7dbe5f539d82fb
just-cmm <1641303361@qq.com>
2021-09-01 14:15:31 +0800

    第一次修改

--- src/main/java/com/example/maven/Hello.java
+++ src/main/java/com/example/maven/Hello.java
@@ -4,5 +4,9 @@ public class Hello {
 	public String sayHello(String name) {
 		return "Hello "+name+"!";
 	}
-	
+public static void main(String[] args) {
+System.out.print("hello");
+System.out.print("第一次");
+
+}
 }
